a52fdab22ca4bc4146be06848462232fed517a80
fzhurd <minganghz@gmail.com>
2026-01-01 22:17:17 -0800

    fix the unit tests

--- resparser/tests/unit/test_skills_extractor.py
+++ resparser/tests/unit/test_skills_extractor.py
@@ -1,18 +1,9 @@
 
-# tests/test_skills_extractor.py
-# from resume_parser.extractors import SkillsExtractor
-# from tests.mocks import MockLLMProvider
-
 from typing import List
 
 from resparser.com.extractor.skills_extractor import SkillsExtractor
 from resparser.com.llm.llm_provider import LLMProvider
 from tests.unit.mocks import MockLLMProvider
-
-
-# class MockLLMProvider(LLMProvider):
-#     def extract_skills(self, resume_text: str) -> List[str]:
-#         return ["Python", "Machine Learning"]
 
 
 def test_skills_extractor_uses_llm():
